a41d91feada90114e45767948b6d4c6ca73e1a4b
Balu Karthik <142300516+Karthi-1211@users.noreply.github.com>
2024-10-07 14:10:43 +0530

    Add files via upload

---
+++ WEEK 1 - C/mario-more/mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int l = 1; l <= i; l++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
