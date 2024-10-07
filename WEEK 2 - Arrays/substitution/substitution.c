7fe03a617bd99ee810adadea95303e00351e062a
Balu Karthik <142300516+Karthi-1211@users.noreply.github.com>
2024-10-07 14:13:40 +0530

    Add files via upload

---
+++ WEEK 2 - Arrays/substitution/substitution.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_valid_key(string s);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if (!is_valid_key(argv[1]))
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+    string plaintext = get_string("plaintext:  "); // Hello!
+    printf("ciphertext: ");
+    for (int i = 0, len = strlen(plaintext); i < len; i++)
+    {
+        if (isalpha(plaintext[i]))
+        {
+            int index = toupper(plaintext[i]) - 'A';
+            if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+                printf("%c", toupper(argv[1][index]));
+            else
+                printf("%c", tolower(argv[1][index]));
+        }
+        else
+            printf("%c", plaintext[i]);
+    }
+    printf("\n");
+}
+
+bool is_valid_key(string s)
+{
+    int len = strlen(s);
+    if (len != 26)
+        return false;
+
+    int freq[26] = {0};
+    for (int i = 0; i < len; i++)
+    {
+        if (!isalpha(s[i]))
+            return false;
+
+        int index = toupper(s[i]) - 'A';
+        if (freq[index] == 1)
+            return false;
+        freq[index]++;
+    }
+    return true;
+}
