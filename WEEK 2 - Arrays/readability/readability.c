af54a18808fafa523223ab2c0b76e29bc337af6c
Balu Karthik <142300516+Karthi-1211@users.noreply.github.com>
2024-10-07 14:13:10 +0530

    Add files via upload

---
+++ WEEK 2 - Arrays/readability/readability.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Text: ");
+
+    int letters, words, sentences;
+    letters = words = sentences = 0;
+
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if (isalpha(s[i]))
+        {
+            letters++;
+        }
+        if ((i == 0 && s[i] != ' ') || (i != i - 1 && s[i] == ' ' && s[i + 1] != ' '))
+        {
+            words++;
+        }
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    float L = letters / (float) words * 100;
+    float S = sentences / (float) words * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
